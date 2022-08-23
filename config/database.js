e406300f01db7b35da8d712b1ba363942078cb72
amycrampton <270066873@yoobeestudent.ac.nz>
2022-08-23 20:56:21 +1200

    database test

--- config/database.js
+++ config/database.js
@@ -1,12 +1,30 @@
 
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const uri = "mongodb+srv://ames28:yoobee01@cluster0.ml1mzyr.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//     if (err) throw err;
+
+// });
+
+// const collection = client.db("amy-test").collection("amy-test-collection");
+
+// module.exports = { collection, ObjectId }
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://ames28:yoobee01@cluster0.ml1mzyr.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = "mongodb+srv://KayZ:kekekeysZw@kayz.njeqifu.mongodb.net/?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
+
+client.connect((err) => {
+
     if (err) throw err;
 
 });
 
-const collection = client.db("amy-test").collection("amy-test-collection");
+const collection = client.db("classly-db").collection("uxDesingers");
+
+
 
 module.exports = { collection, ObjectId }
